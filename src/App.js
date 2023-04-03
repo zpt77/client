ab0760615b1b51fffb279669a9386dfae7895e37
Tomek Zubrzycki <zpt77@wp.pl>
2023-04-03 03:45:35 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username: login, password: password })
+    };
+    fetch('/login', requestOptions)
+      .then(response => response.json())
+      .then(data => setData(data));
+  };
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form className="form">
+        <input
+          className="input"
+          type="text"
+          placeholder="Login"
+          onChange={e => setLogin(e.target.value)}
+        />
+        <input
+          className="input"
+          type="password"
+          placeholder="Password"
+          onChange={e => setPassword(e.target.value)}
+        />
+        <button className="button" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+      {data.length ? (
+        <div>
+          <p>Welcome {data.username}</p>
+        </div>
+      ) : (
+        <p>Please log in</p>
+      )}
     </div>
   );
 }
